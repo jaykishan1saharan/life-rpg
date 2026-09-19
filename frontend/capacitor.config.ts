ba6031b6733e97b7dee3e30c5822df1accb9586e
jaykishan1saharan <jaikishansaharan@gmail.com>
2026-09-20 00:26:52 +0530

    Android version Setup and apk file download

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.jaykishan.liferpg',
+  appName: 'Life RPG',
+  webDir: 'out',
+};
+
+export default config;
